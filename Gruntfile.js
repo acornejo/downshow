33cbfd02a9dbae847b17fbc150426819803ef1d7
Alex Cornejo <acornejo@gmail.com>
2013-06-18 11:27:23 -0400

    Added travis task to grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.registerTask('travis', ['jshint', 'jasmine']);
   grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
 };
 
